1e9a2187e6e31dc549cbaa8192cd9c1c2614dfe7
purab <purabdk@gmail.com>
2021-12-04 10:29:54 -0800

    add...recursion code..

---
+++ java-recursion-tutorials/src/in/purabtech/RecursionTutorial.java
@@ -0,0 +1,15 @@
+package in.purabtech;
+
+public class RecursionTutorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sayHi();		
+	}
+	
+	public static void sayHi() {
+		System.out.println("Hello HI...");
+		sayHi();		
+	}
+
+}

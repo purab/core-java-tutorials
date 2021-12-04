d1ee6db391a9c2947b5f238a5d05d65015074fea
purab <purabdk@gmail.com>
2021-12-04 10:44:28 -0800

    added exit strategy code...

--- java-recursion-tutorials/src/in/purabtech/RecursionTutorial.java
+++ java-recursion-tutorials/src/in/purabtech/RecursionTutorial.java
@@ -4,12 +4,17 @@ public class RecursionTutorial {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		sayHi();		
+		sayHi(3);		
 	}
 	
-	public static void sayHi() {
+	public static void sayHi(int count) {
 		System.out.println("Hello HI...");
-		sayHi();		
+		
+		//added exit strategy for recursive call of method..
+		if(count <=1) {
+			return;
+		}
+		sayHi(count-1);
 	}
 
 }

ea980860a904a6b4eb75a79db613b4cb1e0fb333
purab <purabdk@yahoo.co.in>
2022-01-15 15:26:09 +0530

    added sample for map flatmap demo

--- mapflatmapdemo/src/MapVsFlatMap.java
+++ mapflatmapdemo/src/MapVsFlatMap.java
@@ -7,5 +7,11 @@ public class MapVsFlatMap {
         //list of customers convert list of strings - data tranformation
         List<String> emails = customers.stream().map(customer -> customer.getEmail()).collect(Collectors.toList());
         System.out.println(emails);
+
+        List<List<String>> phoneNumbers = customers.stream().map(customer -> customer.getPhoneNumbers()).collect(Collectors.toList());
+        System.out.println(phoneNumbers);
+
+        List<String> phoneNumbers1 = customers.stream().flatMap(customer -> customer.getPhoneNumbers().stream()).collect(Collectors.toList());
+        System.out.println(phoneNumbers1);
     }
 }

c9801de7b2d2c1c548535d7e5b0f572026c1fd0b
purab <purabdk@yahoo.co.in>
2022-01-17 11:05:12 +0530

    add files...

---
+++ core-java-interview/src/com/purabtech/java/interview/TestOne.java
@@ -0,0 +1,16 @@
+package com.purabtech.java.interview;
+
+public class TestOne {
+
+	public static void main(String[] args) {
+		
+		/* 
+		 * A unicode newline character(\u000d) in Java
+		 */
+		
+		// Line bellow this gives as output
+		//\u000d System.out.println("comment executed");
+
+	}
+
+}
---
+++ core-java-interview/src/com/purabtech/java/interview/TestTwo.java
@@ -0,0 +1,11 @@
+package com.purabtech.java.interview;
+
+public class TestTwo {
+
+	public static void main(String[] args) {
+		String s = "ONE"+ 1+2 +"TWO"+"THREE"+3+4+FOUR"+"FIVE"+5;
+				System.out.println(s);
+
+	}
+
+}
